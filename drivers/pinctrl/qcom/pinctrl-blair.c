76a602fcb8f35ada4d211887c7b45777d2e6c6f2
Giovanni Ricca <giovanniricca@proton.me>
2024-08-21 19:57:19 +0200

    pinctrl: qcom: Remove selected GPIOs from reserved pins list
    From branch: moonstone-s-oss
    
    Change-Id: Ieb4d7dc963598c98cbaa44ab9e1ea466971d264b

--- drivers/pinctrl/qcom/pinctrl-blair.c
+++ drivers/pinctrl/qcom/pinctrl-blair.c
@@ -1593,7 +1593,11 @@ static const struct msm_pingroup blair_groups[] = {
 };
 
 static const int blair_reserved_gpios[] = {
+#ifdef CONFIG_HQ_QGKI
+	13, 14, 15, 16, -1
+#else
 	13, 14, 15, 16, 17, 45, 46, 48, 56, 57, -1
+#endif
 };
 
 static const struct msm_gpio_wakeirq_map blair_mpm_map[] = {
