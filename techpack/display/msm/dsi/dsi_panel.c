b12f99bc3fc93b8c60da962c47081d7693894f0a
Giovanni Ricca <giovanniricca@proton.me>
2024-10-01 15:08:36 +0200

    techpack: display: Implement backlight adjustment for 90 Hz
    Change-Id: I66f20b8181acb7dc23f1071cb74d6672fd3cf0ce

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -1779,6 +1779,7 @@ const char *cmd_set_prop_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-doze-hbm-command",
 	"qcom,mdss-dsi-doze-lbm-command",
 	"qcom,mdss-dsi-dispparam-bc-120hz-command",
+	"qcom,mdss-dsi-dispparam-bc-90hz-command",
 	"qcom,mdss-dsi-dispparam-bc-60hz-command",
 };
 
@@ -1809,6 +1810,7 @@ const char *cmd_set_state_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-doze-hbm-command-state",
 	"qcom,mdss-dsi-doze-lbm-command-state",
 	"qcom,mdss-dsi-dispparam-bc-120hz-command-state",
+	"qcom,mdss-dsi-dispparam-bc-90hz-command-state",
 	"qcom,mdss-dsi-dispparam-bc-60hz-command-state",
 };
 
@@ -4778,6 +4780,8 @@ int dsi_panel_post_switch(struct dsi_panel *panel)
 int dsi_panel_enable(struct dsi_panel *panel)
 {
 	int rc = 0;
+	u8 refresh_rate;
+	enum dsi_cmd_set_type cmd = DSI_CMD_SET_DISP_BC_60HZ;
 
 	if (!panel) {
 		DSI_ERR("Invalid params\n");
@@ -4793,15 +4797,23 @@ int dsi_panel_enable(struct dsi_panel *panel)
 	else
 		panel->panel_initialized = true;
 
-	panel->dsi_refresh_flag = 60;
-	if (panel->cur_mode->timing.refresh_rate == 120) {
-		rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_DISP_BC_120HZ);
-		if (unlikely(rc))
-			DSI_ERR("[%s] failed to send DSI_CMD_SET_DISP_BC_120HZ cmd, rc=%d\n",
-				panel->name, rc);
-		else
-			panel->dsi_refresh_flag = 120;
+	refresh_rate = panel->cur_mode->timing.refresh_rate;
+
+	switch (refresh_rate) {
+		case 120:
+			cmd = DSI_CMD_SET_DISP_BC_120HZ;
+			break;
+		case 90:
+			cmd = DSI_CMD_SET_DISP_BC_90HZ;
+			break;
 	}
+
+	rc = dsi_panel_tx_cmd_set(panel, cmd);
+	if (unlikely(rc))
+		DSI_ERR("[%s] failed to send cmd %d, rc=%d\n",
+			panel->name, cmd, rc);
+	else
+		panel->dsi_refresh_flag = refresh_rate;
 
 	mutex_unlock(&panel->panel_lock);
 	return rc;
@@ -4967,8 +4979,14 @@ inline void dsi_set_backlight_control(struct dsi_panel *panel)
 
 	refresh_rate = panel->cur_mode->timing.refresh_rate;
 
-	if (refresh_rate == 120)
-		cmd = DSI_CMD_SET_DISP_BC_120HZ;
+	switch (refresh_rate) {
+		case 120:
+			cmd = DSI_CMD_SET_DISP_BC_120HZ;
+			break;
+		case 90:
+			cmd = DSI_CMD_SET_DISP_BC_90HZ;
+			break;
+	}
 
 	rc = dsi_panel_tx_cmd_set(panel, cmd);
 
